0caa64611f2695158553671892cb48746a4d3f8f
Rekseto <reksetoajxer@gmail.com>
2017-06-19 00:38:02 +0200

    tooltip component

---
+++ test/index.js
@@ -0,0 +1,27 @@
+// imports
+const path = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+
+
+
+
+const app = express();
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+// uncomment after placing your favicon in /public
+//test.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+module.exports = app;
